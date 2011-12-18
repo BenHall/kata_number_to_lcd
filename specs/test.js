0cbe740e0275f37baf3f9a5184e39445cfc480cf
BenHall <Ben@Benhall.me.uk>
2011-12-18 15:40:32 +0000

    Numbers one and two

--- specs/test.js
+++ specs/test.js
@@ -1,4 +1,30 @@
-module("LCD Display");
+module("LCD Display - Output");
 
-test("", function() {
+var logOutput = undefined
+QUnit.begin = function() {
+  window.console.log = function(output) { 
+    logOutput = output;
+  };
+}
+
+test("Number 1", function() {
+  var d = new LCDDisplay();
+  d.output(1);
+
+  equals(logOutput, "\n" + 
+                    "|\n" +
+                    "|\n" + 
+                    "|\n"+
+                    "|\n");
+});
+
+test("Number 2", function() {
+  var d = new LCDDisplay();
+  d.output(2);
+
+  equals(logOutput, "__\n" +
+                    "   |\n" +
+                    " __|\n" +
+                    "|\n" +
+                    "|__\n");
 });
--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 var LCDDisplay = function() {};
 
 LCDDisplay.prototype.output = function(n) {
+  var oneOutline = ["", "|", "|", "|", "|"];
+  var twoOutline = ["__", "   |", " __|", "|","|__"];
+
+  var result = "";
+  
+  for(var i = 0; i < 5; i++) {
+    if(n == 1) 
+      result += oneOutline[i] + "\n";
+    else
+      result += twoOutline[i] + "\n";
+  }
+
+  console.log(result);
 }
